62fd2ec6cb5bab065618e268f2d7581b0601a9f9
Luciana001 <luciana.brancato@gmail.com>
2021-04-21 12:39:40 +0200

    create card

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const boxen = require("boxen");
+const chalk = require("chalk");
+    //const EMPTYLINE = "";
+const NewLine = "\n";
+    
+console.log(
+    boxen(
+        [
+            chalk.cyan.bold('Name:')+chalk.white.bold( ' Luciana Brancato'),
+            chalk.cyan.bold('Work:')+chalk.redBright(' Web developper Junior'),
+            '-------------------------------------------------------------',
+            chalk.cyan.bold('Email:')+ ' luciana.brancato@gmail.com',
+            chalk.cyan.bold('GitHub:')+ chalk.greenBright(' https://github.com/Luciana001'),
+            '-------------------------------------------------------------',
+            chalk.cyan.bold('Discord:')+ chalk.magenta(' #9549')
+        ].join(NewLine),
+        {
+            //align:center,
+            padding: 1,
+            margin: 1,
+            borderStyle: "round",
+            borderColor:"yellowBright"
+        },
+    )
+)
