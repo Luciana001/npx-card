fc96778a0d0599b740ead1536aa2f02065db6be9
Luciana001 <luciana.brancato@gmail.com>
2021-04-21 14:16:10 +0200

    design final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const boxen = require("boxen");
 const chalk = require("chalk");
-    //const EMPTYLINE = "";
+const EMPTYLINE = "";
 const NewLine = "\n";
     
 console.log(
@@ -9,14 +9,16 @@ console.log(
         [
             chalk.cyan.bold('Name:')+chalk.white.bold( ' Luciana Brancato'),
             chalk.cyan.bold('Work:')+chalk.redBright(' Web developper Junior'),
-            '-------------------------------------------------------------',
+            '-----------------',
             chalk.cyan.bold('Email:')+ ' luciana.brancato@gmail.com',
             chalk.cyan.bold('GitHub:')+ chalk.greenBright(' https://github.com/Luciana001'),
-            '-------------------------------------------------------------',
-            chalk.cyan.bold('Discord:')+ chalk.magenta(' #9549')
+            '-----------------',
+            chalk.cyan.bold('Discord:')+ chalk.magenta(' #9549'),
+            EMPTYLINE,
+            chalk.cyan.bold('Card:')+ chalk.white.bold( '@luciana001/card')
         ].join(NewLine),
         {
-            //align:center,
+            align:"center",
             padding: 1,
             margin: 1,
             borderStyle: "round",
